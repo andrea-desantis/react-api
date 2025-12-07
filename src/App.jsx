6c02d72efb5d631efb482b40615f41ea72249146
andrea-desantis <andrea.desantis290@gmail.com>
2025-12-07 19:41:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+
+function App() {
+  
+  axios
+    .get(
+      " https://lanciweb.github.io/demo/api/actresses/"
+    )
+    .then(() => {
+
+    });
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
